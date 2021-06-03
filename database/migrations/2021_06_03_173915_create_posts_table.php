a97a97d4af8f9d6c570514874edf9d2471fda494
alexakbar <alexampp11@gmail.com>
2021-06-04 00:52:33 +0700

    create table structure

---
+++ database/migrations/2021_06_03_173915_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->string("image");
+            $table->text("text")->nullable();
+            $table->boolean("is_turn_of_comment")->default(0);
+            $table->boolean("is_turn_of_like")->default(0);
+            $table->timestamps();
+
+            $table
+            ->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
